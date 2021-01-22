44407bbb8312a57842e5f6061b65381c34b340af
me <myself@myself.com>
2021-01-22 14:36:21 +0000

    React Login

--- src/App.js
+++ src/App.js
@@ -1,75 +1,101 @@
-import React from "react";
-import GoogleLogin from "react-google-login";
-import { GoogleLogout } from "react-google-login";
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 
-import "./App.scss";
+import './App.scss';
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      userDetails: {},
-      isUserLoggedIn: false
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			userDetails: {},
+			isUserLoggedIn: false,
+		};
+	}
 
-  responseGoogle = response => {
-    this.setState({ userDetails: response.profileObj, isUserLoggedIn: true });
-  };
+	componentDidMount() {
+		const details = localStorage.getItem('userDetails');
+		console.log(JSON.parse(details));
+		if (details) {
+			this.setState({ userDetails: JSON.parse(details), isUserLoggedIn: true });
+		}
+	}
 
-  logout = () => {
-    this.setState({isUserLoggedIn: false})
-  };
+	responseGoogle = (response) => {
+		console.log(response.profileObj);
+		localStorage.setItem('userDetails', JSON.stringify(response.profileObj));
+		this.setState({ userDetails: response.profileObj, isUserLoggedIn: true });
+	};
 
-  render() {
-    return (
-      <div className="App">
-        {!this.state.isUserLoggedIn && (
-          <GoogleLogin
-            clientId="" //TO BE CREATED
-            render={renderProps => (
-              <button
-                className="button"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-              >
-                Log in with Google
-              </button>
-            )}
-            onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-          />
-        )}
-        {this.state.isUserLoggedIn && (
-          <div className="userDetails-wrapper">
-            <div className="details-wrapper">
-              <GoogleLogout
-                render={renderProps => (
-                  <button
-                    className="logout-button"
-                    onClick={renderProps.onClick}
-                  >
-                    Log Out
-                  </button>
-                )}
-                onLogoutSuccess={this.logout}
-              />
+	errorResponse = (response) => {
+		alert("Failed to login");
+	};
 
-              <div className="image">
-                <img src={this.state.userDetails.imageUrl} />
-              </div>
-              <div className="name">
-                Welcome Mr. {this.state.userDetails.givenName}{" "}
-                {this.state.userDetails.familyName}
-              </div>
-              <div className="email"><i>{this.state.userDetails.email}</i></div>
-            </div>
-            <div className="bar" />
-            <div className="stand" />
-          </div>
-        )}
-      </div>
-    );
-  }
+	logout = () => {
+		localStorage.clear();
+		this.setState({ userDetails: {}, isUserLoggedIn: false });
+	};
+
+	render() {
+		if (!this.state.isUserLoggedIn) {
+			return (
+				<div className="App">
+					<div className="box">
+						<h2>BIT Wifi</h2>
+						<GoogleLogin
+							clientId="901118527459-ftq3sn8gtdoihl62alnjrbonubt5qrlp.apps.googleusercontent.com"
+							render={(renderProps) => (
+								<button
+									className="button"
+									onClick={renderProps.onClick}
+									disabled={renderProps.disabled}
+								>
+									<img
+										src="icons/google.svg"
+										alt="google login"
+										className="icon"
+									></img>
+									<span className="buttonText">Sign in with Google</span>
+								</button>
+							)}
+							onSuccess={this.responseGoogle}
+							onFailure={this.errorResponse}
+						/>
+					</div>
+				</div>
+			);
+		}
+
+		return (
+			<div className="App">
+				<div className="userDetails-wrapper">
+					<div className="details-wrapper">
+						<GoogleLogout
+							clientId="901118527459-ftq3sn8gtdoihl62alnjrbonubt5qrlp.apps.googleusercontent.com"
+							render={(renderProps) => (
+								<button className="logout-button" onClick={renderProps.onClick}>
+									Log Out
+								</button>
+							)}
+							onLogoutSuccess={this.logout}
+						/>
+
+						<div className="image">
+							<img src={this.state.userDetails.imageUrl} alt="User" />
+						</div>
+						<div className="name">
+							Welcome Mr. {this.state.userDetails.givenName}{' '}
+							{this.state.userDetails.familyName}
+						</div>
+						<div className="email">
+							<i>{this.state.userDetails.email}</i>
+						</div>
+					</div>
+					{/*<div className="bar" />
+					<div className="stand" />*/}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
